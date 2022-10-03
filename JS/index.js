214ce4fba22051e0f4e33157e353afe99c51ac42
Julian Hirt <julian.hirt94@web.de>
2022-10-03 17:26:59 +0200

    redesign my personal website

---
+++ JS/index.js
@@ -0,0 +1,23 @@
+const display = document.querySelector('[data-js="display"]');
+const time = new Date().getHours();
+const day = new Date().getDay();
+
+function getGreeting() {
+  if (time > 6 && time < 12) {
+    return "Good Morning";
+  } else if (time > 12 && time < 18) {
+    return "Good Afternoon";
+  } else if (time > 18 && time < 22) {
+    return "Good Evening";
+  } else if (time > 23 && time < 5) {
+    return "Good Night";
+  }
+}
+
+display.textContent = getGreeting();
+
+const togglebutton = document.querySelector('[data-js="switch"]');
+
+toggleButton.addEventListener("change", () => {
+  bodyElement.classList.add(".dark");
+});
